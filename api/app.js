95f0df476424a089cbf26f66a6b8f5bafbb4bc63
Antonio-Melo <happyd3nt@gmail.com>
2017-11-26 22:38:51 +0000

    testing basics

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 
 const apiRouter = require('./routes/api');
+var programmeRouter = require('./routes/programme');
 
 const app = express();
 
@@ -11,7 +12,30 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Connect to Date Base
+var config = require('config');
+var dbConfig = config.get('dbConfig');
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : dbConfig.host,
+  user     : dbConfig.username,
+  password : dbConfig.password,
+  database : dbConfig.dbName
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+//Routers
 app.use('/api', apiRouter);
+app.use('/api/programme', programmeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- api/routes/api.js
+++ api/routes/api.js
@@ -1,38 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var config = require('config');
-var dbConfig = config.get('dbConfig');
-console.log(dbConfig);
-
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host     : dbConfig.host,
-  user     : dbConfig.username,
-  password : dbConfig.password,
-  database : dbConfig.dbName
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-
-  console.log('connected as id ' + connection.threadId);
-});
 
 router.get('/', function(req, res, next) {
   res.json('TTS API Home');
-});
-
-router.get('/programme', function(req, res, next) {
-  const programmes = ['FEUP-MIEIC', 'FEUP-MIEEC'];
-  res.json(programmes);
-})
-
-router.get('/schedule-data', function(req, res, next) {
-  res.sendfile('./testdata.json');
 });
 
 module.exports = router;
